c12ce09bc2a9beaf227c1f8321a8b0041adf6ee6
Taylor Otwell <taylorotwell@gmail.com>
2012-08-10 20:30:11 -0500

    added the array functions.

--- src/helpers.php
+++ src/helpers.php
@@ -27,6 +27,91 @@ function array_dot($array, $prepend = '')
 }
 
 /**
+ * Get an item from an array using "dot" notation.
+ *
+ * @param  array   $array
+ * @param  string  $key
+ * @param  mixed   $default
+ * @return mixed
+ */
+function array_get($array, $key, $default = null)
+{
+	if (is_null($key)) return $array;
+
+	foreach (explode('.', $key) as $segment)
+	{
+		if ( ! is_array($array) or ! array_key_exists($segment, $array))
+		{
+			return value($default);
+		}
+
+		$array = $array[$segment];
+	}
+
+	return $array;
+}
+
+/**
+ * Set an array item to a given value using "dot" notation.
+ *
+ * If no key is given to the method, the entire array will be replaced.
+ *
+ * @param  array   $array
+ * @param  string  $key
+ * @param  mixed   $value
+ * @return void
+ */
+function array_set(&$array, $key, $value)
+{
+	if (is_null($key)) return $array = $value;
+
+	$keys = explode('.', $key);
+
+	while (count($keys) > 1)
+	{
+		$key = array_shift($keys);
+
+		// If the key doesn't exist at this depth, we will just create an empty array
+		// to hold the next value, allowing us to create the arrays to hold final
+		// values at the correct depth. Then we'll keep digging into the array.
+		if ( ! isset($array[$key]) or ! is_array($array[$key]))
+		{
+			$array[$key] = array();
+		}
+
+		$array =& $array[$key];
+	}
+
+	$array[array_shift($keys)] = $value;
+}
+
+/**
+ * Remove an array item from a given array using "dot" notation.
+ *
+ * @param  array   $array
+ * @param  string  $key
+ * @return void
+ */
+function array_forget(&$array, $key)
+{
+	$keys = explode('.', $key);
+
+	while (count($keys) > 1)
+	{
+		$key = array_shift($keys);
+
+		if ( ! isset($array[$key]) or ! is_array($array[$key]))
+		{
+			return;
+		}
+
+		$array =& $array[$key];
+	}
+
+	unset($array[array_shift($keys)]);
+}
+
+/**
  * Determine if a string starts with a given needle.
  *
  * @param  string  $haystack
@@ -49,7 +134,7 @@ function str_is($pattern, $value)
 {
 	// Asterisks are translated into zero-or-more regular expression wildcards
 	// to make it convenient to check if the strings starts with the given
-	// pattern such as "library/*". Useful for checking basic strings.
+	// pattern such as "library/*", making any string check convenient.
 	if ($pattern !== '/')
 	{
 		$pattern = str_replace('*', '(.*)', $pattern).'\z';
--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -9,6 +9,32 @@ class HelpersTest extends PHPUnit_Framework_TestCase {
 	}
 
 
+	public function testArrayGet()
+	{
+		$array = array('names' => array('developer' => 'taylor'));
+		$this->assertEquals('taylor', array_get($array, 'names.developer'));
+		$this->assertEquals('dayle', array_get($array, 'names.otherDeveloper', 'dayle'));
+		$this->assertEquals('dayle', array_get($array, 'names.otherDeveloper', function() { return 'dayle'; }));
+	}
+
+
+	public function testArraySet()
+	{
+		$array = array();
+		array_set($array, 'names.developer', 'taylor');
+		$this->assertEquals('taylor', $array['names']['developer']);
+	}
+
+
+	public function testArrayForget()
+	{
+		$array = array('names' => array('developer' => 'taylor', 'otherDeveloper' => 'dayle'));
+		array_forget($array, 'names.developer');
+		$this->assertFalse(isset($array['names']['developer']));
+		$this->assertTrue(isset($array['names']['otherDeveloper']));
+	}
+
+
 	public function testStrIs()
 	{
 		$this->assertTrue(str_is('*.dev', 'localhost.dev'));
