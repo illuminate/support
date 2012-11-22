9f95b1bf7cfb5ba7a31fb4511033ffc22d113246
Taylor Otwell <taylorotwell@gmail.com>
2012-11-21 20:36:49 -0600

    work on resource registration.

--- src/Illuminate/Support/ServiceProvider.php
+++ src/Illuminate/Support/ServiceProvider.php
@@ -3,6 +3,13 @@
 abstract class ServiceProvider {
 
 	/**
+	 * The view engine used by the package's views.
+	 *
+	 * @var string
+	 */
+	protected $viewEngine = 'php';
+
+	/**
 	 * Indicates if the service provider is deferred.
 	 *
 	 * @var bool
@@ -24,6 +31,43 @@ abstract class ServiceProvider {
 	 * @return void
 	 */
 	abstract public function register($app);
+
+	/**
+	 * Register the package's component namespaces.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @param  string  $package
+	 * @param  string  $path
+	 * @return void
+	 */
+	public function registerComponents($app, $vendor, $package, $path)
+	{
+		// In this method we will register the configuration package for the package
+		// so that the configuration options cleanly cascade into the application
+		// folder to make the developers lives much easier in maintaining them.
+		if ($app['files']->isDirectory($config = $path.'/config'))
+		{
+			$app['config']->package($vendor.'/'.$package, $config);
+		}
+
+		// Next we will check for any "language" components. If language files exist
+		// we will register them with this given package's namespace so that they
+		// may be accessed using the translation facilities of the application.
+		if ($app['files']->isDirectory($lang = $path.'/lang'))
+		{
+			$app['translator']->addNamespace($package, $lang);
+		}
+
+		// Finally we will register the view namespace so that we can access each of
+		// the views available in this package. We use a standard convention when
+		// registering the paths to every package's views and other components.
+		if ($app['files']->isDirectory($view = $path.'/views'))
+		{
+			$views = $app['view']->driver($this->viewEngine);
+
+			$views->addNamespace($package, $view);
+		}
+	}
 
 	/**
 	 * Get the services provided by the provider.
