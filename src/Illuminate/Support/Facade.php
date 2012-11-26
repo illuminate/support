a643eeaf4a453204637f8ba456df61f7ccf0188b
Taylor Otwell <taylorotwell@gmail.com>
2012-11-26 15:32:59 -0600

    improve facade performance.

--- src/Illuminate/Support/Facade.php
+++ src/Illuminate/Support/Facade.php
@@ -10,6 +10,13 @@ abstract class Facade {
 	protected static $app;
 
 	/**
+	 * The resolved object instance.
+	 *
+	 * @var mixed
+	 */
+	protected static $resolvedInstance;
+
+	/**
 	 * Get the registered name of the component.
 	 *
 	 * @return string
@@ -17,6 +24,22 @@ abstract class Facade {
 	protected static function getFacadeAccessor()
 	{
 		throw new \RuntimeException("Facade does not implement getFacadeAccessor method.");
+	}
+
+	/**
+	 * Resolve the facade root instance from the container.
+	 *
+	 * @param  string  $name
+	 * @return mixed
+	 */
+	protected static function resolveFacadeInstance($name)
+	{
+		if (isset(static::$resolvedInstance))
+		{
+			return static::$resolvedInstance;
+		}
+
+		return static::$resolvedInstance = static::$app[$name];
 	}
 
 	/**
@@ -44,16 +67,35 @@ abstract class Facade {
 	 * Handle dynamic, static calls to the object.
 	 *
 	 * @param  string  $method
-	 * @param  array   $parameters
+	 * @param  array   $args
 	 * @return mixed
 	 */
-	public static function __callStatic($method, $parameters)
+	public static function __callStatic($method, $args)
 	{
 		$name = static::getFacadeAccessor();
 
-		$instance = is_object($name) ? $name : static::$app[$name];
+		$instance = static::resolveFacadeInstance($name);
 
-		return call_user_func_array(array($instance, $method), $parameters);
+		switch (count($args))
+		{
+			case 0:
+				return $instance->$method();
+
+			case 1:
+				return $instance->$method($args[0]);
+
+			case 2:
+				return $instance->$method($args[0], $args[1]);
+
+			case 3:
+				return $instance->$method($args[0], $args[1], $args[2]);
+
+			case 4:
+				return $instance->$method($args[0], $args[1], $args[2], $args[3]);
+
+			default:
+				return call_user_func_array(array($instance, $method), $args);
+		}
 	}
 
 }
