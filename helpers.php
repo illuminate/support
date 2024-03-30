717a5855c770ef66192f5b9448060b1f576c6e93
Arshia Montakhabi <armoteamco@gmail.com>
2024-03-30 12:12:22 +0330

    perf: Simplify logic with dedicated helper function 'blank'

--- helpers.php
+++ helpers.php
@@ -43,20 +43,12 @@ if (! function_exists('blank')) {
      */
     function blank($value)
     {
-        if (is_null($value)) {
-            return true;
-        }
-
         if (is_string($value)) {
             return trim($value) === '';
         }
 
         if (is_numeric($value) || is_bool($value)) {
             return false;
-        }
-
-        if ($value instanceof Countable) {
-            return count($value) === 0;
         }
 
         return empty($value);
